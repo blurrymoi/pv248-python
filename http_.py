c5e7828111eb8b75286e6b1083051e2cafdc33dd
blurry <blurry@localhost.localdomain>
2017-12-20 10:18:48 +0100

    ex08: http2 (partial, sans html)

--- http_.py
+++ http_.py
@@ -1,7 +1,8 @@
 import http.client
 from http.server import *
-import ssl  # SSL/TLS wrapper
-
+import urllib.parse
+from subprocess import call
+import sys
 
 class RequestHandler(BaseHTTPRequestHandler):
 
@@ -10,10 +11,30 @@ class RequestHandler(BaseHTTPRequestHandler):
         # pass a CLASS (not instance) to HTTPServer
         # request data available in instance variables self.path, self.headers
         self.send_response(200, 'OK')
-        self.send_header('Content-Type', 'text/plain')
+        d = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        print(d)
+        header = 'text/html' if 'html' in d['f'] else 'application/json'
+
+        self.send_header('Content-Type', '%s' % header)
         self.end_headers()
-        self.wfile.write(bytes("{} faktorial".format(self.path[1:]), 'utf-8'))
+        q = [x[-1] for x in d['q'] if x.startswith('search')][0]
+        print(q)
+
+        html = ''' <!DOCTYPE html><html><body>
+            <h2> Result for q</h2>
+            <ul style="list-style-type:circle">'''
+#        for item in call(["python3", "search.py", q], stdout=fil):
+
+        if 'f' in d and 'json' in d['f']:
+            call(["python3", "search.py", q], stdout=self.wfile)
+        elif 'f' in d and 'html' in d['f']:
+            self.wfile.write(bytes(html, 'utf-8'))
+            #call(["python3", "search.py", q], stdout=self.wfile)
+
+        # self.wfile.write(bytes("{} faktorial".format(self.path[1:]), 'utf-8'))
         # sockets only accept byte sequences, not str
+
+# something about html webform
 
 
 def run(server_class=HTTPServer, handler_class=RequestHandler):
